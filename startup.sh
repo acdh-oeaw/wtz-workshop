6657efa2bfa956d1579235fe22f4339fc6059473
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2017-05-18 11:51:49 +0200

    moved spacy download command to bootstrap and removed the model from the requirements

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-su - vagrant -c "source .venvs/myenv/bin/activate && python -m spacy download de && nohup jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --notebook-dir=/home/vagrant/notebook --NotebookApp.token='' &"
+su - vagrant -c "source .venvs/myenv/bin/activate && nohup jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --notebook-dir=/home/vagrant/notebook --NotebookApp.token='' &"
